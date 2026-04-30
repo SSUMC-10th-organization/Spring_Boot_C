6d8153817eab39e68fa63147a44472421f03a0dd
wonellyho <navybsktb@gmail.com>
2026-04-30 11:07:50 +0900

    mission/#10 - 공통 ApiResponse 수정

--- umc10th/src/main/java/com/example/umc10th/domain/member/service/MemberService.java
+++ umc10th/src/main/java/com/example/umc10th/domain/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.example.umc10th.domain.member.service;
 
 import com.example.umc10th.domain.member.converter.MemberConverter;
 import com.example.umc10th.domain.member.dto.MemberReqDTO;
+import com.example.umc10th.domain.member.dto.MemberResDTO;
 import com.example.umc10th.domain.member.exception.MemberException;
 import com.example.umc10th.domain.member.exception.code.MemberErrorCode;
 
--- umc10th/src/main/java/com/example/umc10th/global/apiPayload/ApiResponse.java
+++ umc10th/src/main/java/com/example/umc10th/global/apiPayload/ApiResponse.java
@@ -1,13 +1,12 @@
 package com.example.umc10th.global.apiPayload;
 
 import com.example.umc10th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
-// ↑ JSON으로 변환할 때 이 순서로 출력
 public class ApiResponse<T> {
-//                      ↑ T = 어떤 타입이든 올 수 있음 (제네릭)
 
     @JsonProperty("isSuccess")
     private final Boolean isSuccess;  // 성공 여부
@@ -21,13 +20,31 @@ public class ApiResponse<T> {
     @JsonProperty("result")
     private T result;                 // 실제 데이터 (성공시 데이터, 실패시 에러정보)
 
-    // 실패한 경우 ApiResponse 만들기
+
+    public ApiResponse(Boolean isSuccess, String code, String message, T result) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+        this.result=result;
+    }
+
+    // 성공한 경우
+    public static <T> ApiResponse<T> onSuccess(BaseSuccessCode code, T result){
+        return new ApiResponse<>(
+                true,
+                code.getCode(),
+                code.getMessage(),
+                result
+        );
+    }
+
+    // 실패한 경우 ApiResponse 만들기, 상태코드 나타낼때 code, 실패결과가 있다면 담을 result
     public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result) {
         return new ApiResponse<>(
                 false,              // isSuccess = false
                 code.getCode(),     // 에러코드에서 code 꺼냄
                 code.getMessage(),  // 에러코드에서 message 꺼냄
-                result              // 에러 상세 정보
+                result         // 에러 상세 정보
         );
     }
 }
